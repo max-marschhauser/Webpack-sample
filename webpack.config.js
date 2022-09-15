48129316d47697a7904f003d62945c9ef6ef6dcf
max-marschhauser <105870867+max-marschhauser@users.noreply.github.com>
2022-09-15 12:41:14 +0200

    sample

---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+	mode: "development",
+	entry: {
+		bundle: path.resolve(__dirname, "src/index.js"),
+	},
+	output: {
+		path: path.resolve(__dirname, "dist"),
+		filename: "[name][contenthash].js", // može se maknuti [contenthast] ako ne želim da bude cashing i hash kod imena filea
+		clean: true,
+		assetModuleFilename: "[name][ext]",
+	},
+	devtool: "source-map",
+	devServer: {
+		static: {
+			directory: path.resolve(__dirname, "dist"),
+		},
+		port: 3000,
+		open: true,
+		hot: true,
+		compress: true,
+		historyApiFallback: true,
+	},
+	module: {
+		rules: [
+			{
+				test: /\.scss$/,
+				use: ["style-loader", "css-loader", "sass-loader"],
+			},
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: "babel-loader",
+					options: {
+						presets: ["@babel/preset-env"],
+					},
+				},
+			},
+			{
+				test: /\.(png|svg|jpg|jpeg|gif)$/i,
+				type: "asset/resource",
+			},
+		],
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			title: "Webpack App", // change title here or delete and change title in html file
+			filename: "index.html",
+			template: "src/template.html",
+		}),
+	],
+};
